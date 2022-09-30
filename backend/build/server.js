e342d84b507ed04dc4c4b1dfe85c31b44c936189
Can İlgu <canilguu@gmail.com>
2022-09-30 14:30:37 +0300

    express initialized

---
+++ backend/build/server.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable no-console */
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3001;
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from typescript express!');
+});
+app.listen(port, () => {
+    console.log(`Node app listening on port ${port}`);
+});
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello from typescript!")
+/* eslint-disable no-console */
+import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+const app: Application = express();
+
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Hello from typescript express!');
+});
+
+app.listen(port, () => {
+  console.log(`Node app listening on port ${port}`);
+});
