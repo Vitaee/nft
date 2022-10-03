18b7dd97143adcd5e1746d6a8bf236faa87f99d7
Can İlgu <canilguu@gmail.com>
2022-10-03 09:25:15 +0300

    test example with supertest

--- backend/src/server.spec.ts
+++ backend/src/server.spec.ts
@@ -1,5 +1,15 @@
-describe('test', () => {
-    test('add', async () => {
-      expect(1 + 1).toEqual(2);
-    });
+/* eslint-disable no-console */
+/* eslint-disable prettier/prettier */
+import app from './server';
+import supertest from "supertest";
+describe('Testing Express Api', () => {
+
+  it('Tests the base route', async () => {
+
+    const response = await supertest(app).get('/');
+
+		expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message');
   });
+
+});
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,16 @@
 /* eslint-disable no-console */
 import express, { Application, Request, Response } from 'express';
-import mongoose from 'mongoose';
 
 const app: Application = express();
 
 const port = 3001;
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Hello from typescript express!');
+  res.status(200).send({ message: 'Express app running well!' });
 });
 
 app.listen(port, () => {
   console.log(`Node app listening on port ${port}`);
 });
+
+export default app;
